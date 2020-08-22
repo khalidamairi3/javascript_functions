32134a7fbbbc34f3da47fb1559cc6102928fc03f
khalidamairi3 <khalidamairi3@gmail.com>
2020-08-22 11:20:55 -0600

    W9E functions top

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
+function first_function (num1,num2){
+    var sum =num1+num2;
+    var result = sum*5;
+    return result;
+}
+
+function second_function(string){
+    return string.length > 10;
+}
+
+function third_function(array){
+    
+    var ph_strings=array.filter(string =>string.startsWith("ph"))
+    return ph_strings[0];
+}
+
+
 console.log(first_function (2,1));
 console.log(first_function (12,8));
 console.log(first_function (3,7));
@@ -16,21 +33,7 @@ console.log(third_function(["history","biology","philosophy","english","math","p
 
 
 
-function first_function (num1,num2){
-    var sum =num1+num2;
-    var result = sum*5;
-    return result;
-}
 
-function second_function(string){
-    return string.length > 10;
-}
-
-function third_function(array){
-    
-    var ph_strings=array.filter(string =>string.startsWith("ph"))
-    return ph_strings[0];
-}
 
 
 
